275380c843037093e8b391c11267a7a991716688
MaciejWojs <MaciejWojs@users.noreply.github.com>
2024-10-13 17:30:32 +0000

    deploy: 3a26760089b709932462e0769b5e1d4012260df3

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['push_5fback_0',['push_back',['../classList.html#a3b4bc7220b3c43c76baa34f2aa1386e2',1,'List']]],
-  ['push_5ffront_1',['push_front',['../classList.html#afd44dfbf7059504174ef59567acbd1c8',1,'List']]]
+  ['purge_0',['purge',['../classList.html#a912c199604dc1385e192530e81309c35',1,'List']]],
+  ['push_5fback_1',['push_back',['../classList.html#a3b4bc7220b3c43c76baa34f2aa1386e2',1,'List']]],
+  ['push_5ffront_2',['push_front',['../classList.html#afd44dfbf7059504174ef59567acbd1c8',1,'List']]]
 ];
