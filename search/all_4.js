67f7bdb72eef249919c3b30b3b10a960638b5216
MaciejWojs <MaciejWojs@users.noreply.github.com>
2024-10-13 18:26:19 +0000

    deploy: 59f07b69ad4253dcd0b67b477ce083d1426fb259

--- search/all_4.js
+++ search/all_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['operator_3c_3c_0',['operator&lt;&lt;',['../classList.html#a47aa3457652d0536017505f280c9d010',1,'List::operator&lt;&lt;'],['../List_8cpp.html#a47aa3457652d0536017505f280c9d010',1,'operator&lt;&lt;():&#160;List.cpp']]]
+  ['operator_3c_3c_0',['operator&lt;&lt;',['../classList.html#a3a03ab727b0c8cca288ba5541ac19bc6',1,'List::operator&lt;&lt;'],['../classList.html#a47aa3457652d0536017505f280c9d010',1,'List::operator&lt;&lt;'],['../List_8cpp.html#a47aa3457652d0536017505f280c9d010',1,'operator&lt;&lt;(std::ostream &amp;stream, const List::Node *node):&#160;List.cpp'],['../List_8cpp.html#a3a03ab727b0c8cca288ba5541ac19bc6',1,'operator&lt;&lt;(std::ostream &amp;stream, const List &amp;list):&#160;List.cpp']]],
+  ['operator_5b_5d_1',['operator[]',['../classList.html#afb14d1b5bfad6e2e47b12272a99f9179',1,'List']]]
 ];
--- search/related_0.js
+++ search/related_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_3c_3c_0',['operator&lt;&lt;',['../classList.html#a47aa3457652d0536017505f280c9d010',1,'List']]]
+  ['operator_3c_3c_0',['operator&lt;&lt;',['../classList.html#a3a03ab727b0c8cca288ba5541ac19bc6',1,'List::operator&lt;&lt;'],['../classList.html#a47aa3457652d0536017505f280c9d010',1,'List::operator&lt;&lt;']]]
 ];
