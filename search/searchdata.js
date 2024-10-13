f56655ef9c13d5b40ea208f862d1c4185c428a29
MaciejWojs <MaciejWojs@users.noreply.github.com>
2024-10-13 12:45:12 +0000

    deploy: 0aed551e72eddae662142d42ef294b9c595bd002

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "l~",
+  0: "lr~",
   1: "l",
-  2: "l~"
+  2: "l~",
+  3: "r"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions"
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions"
+  2: "Functions",
+  3: "Pages"
 };
 
