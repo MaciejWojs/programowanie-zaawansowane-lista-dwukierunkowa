62564be514b2ad1bf827d0e29eb28ec9e70ba497
MaciejWojs <MaciejWojs@users.noreply.github.com>
2024-10-13 17:41:07 +0000

    deploy: a7411838bd4758c93bf972d82f8798a68716a8e6

--- search/all_5.js
+++ search/all_5.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['readme_0',['README',['../md_README.html',1,'']]],
   ['readme_2emd_1',['README.md',['../README_8md.html',1,'']]],
-  ['remove_5ffirst_2',['remove_first',['../classList.html#ad83b758e6dfa0a39e627b2efedb7f284',1,'List']]],
-  ['remove_5flast_3',['remove_last',['../classList.html#a5d0cccb611078256a7bfd2b6a6e26532',1,'List']]]
+  ['remove_5fat_2',['remove_at',['../classList.html#a56cab1ddab97be01968aed50f0412be7',1,'List']]],
+  ['remove_5ffirst_3',['remove_first',['../classList.html#ad83b758e6dfa0a39e627b2efedb7f284',1,'List']]],
+  ['remove_5flast_4',['remove_last',['../classList.html#a5d0cccb611078256a7bfd2b6a6e26532',1,'List']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['remove_5ffirst_0',['remove_first',['../classList.html#ad83b758e6dfa0a39e627b2efedb7f284',1,'List']]],
-  ['remove_5flast_1',['remove_last',['../classList.html#a5d0cccb611078256a7bfd2b6a6e26532',1,'List']]]
+  ['remove_5fat_0',['remove_at',['../classList.html#a56cab1ddab97be01968aed50f0412be7',1,'List']]],
+  ['remove_5ffirst_1',['remove_first',['../classList.html#ad83b758e6dfa0a39e627b2efedb7f284',1,'List']]],
+  ['remove_5flast_2',['remove_last',['../classList.html#a5d0cccb611078256a7bfd2b6a6e26532',1,'List']]]
 ];
